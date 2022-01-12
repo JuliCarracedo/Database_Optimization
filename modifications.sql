3a6042a16e288cb8eaac382d3dc8ae9ddd59261f
JuliCarracedo <julicarra@hotmail.com>
2022-01-12 18:24:26 -0300

    fix typos in sql modifications

--- modifications.sql
+++ modifications.sql
@@ -6,7 +6,7 @@ CREATE INDEX index_animal_id ON visits(animal_id);
 
 -- Index the vet_id variable and then cluster the table based on it to improve query speed.
 CREATE INDEX index_vet_id ON visits(vet_id);
-CLUSTER visits USING idx_vet_id;
+CLUSTER visits USING index_vet_id;
 
 -- Index the index_owners_email variable to improve query speed
 CREATE INDEX index_owners_email ON owners(email);
